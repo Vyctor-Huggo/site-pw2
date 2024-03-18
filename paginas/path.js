53995820e950c524d4f06cc0f8042ddd958df848
Vyctor Huggo <74301628+Vyctor-Huggo@users.noreply.github.com>
2024-03-18 11:23:40 -0300

    Update path.js

--- paginas/path.js
+++ paginas/path.js
@@ -1,7 +1,7 @@
 var paths = {
-    "inicial": "/Inicial/inicial.html",
-    "ingressos": "/Ingressos/ingressos.html",
-    "loja": "/Loja/loja.html",
+    "inicial": "../Inicial/inicial.html",
+    "ingressos": "../Ingressos/ingressos.html",
+    "loja": "../Loja/loja.html",
     "contatos": "#Footer",
     "instagram": "https://www.instagram.com/gatos_fofos_e_engracados/",
     "twitter": "https://twitter.com/gatinarios"
