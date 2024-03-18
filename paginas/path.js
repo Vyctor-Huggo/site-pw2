db513adbd9fe2ba2c16ced3aa9ea07280538cd15
Miguel <126696785+MiguelVascoDevWeb@users.noreply.github.com>
2024-03-18 17:53:26 -0300

    Versão mais atualizada

--- paginas/path.js
+++ paginas/path.js
@@ -1,7 +1,7 @@
 var paths = {
-    "inicial": "../Inicial/inicial.html",
-    "ingressos": "../Ingressos/ingressos.html",
-    "loja": "../Loja/loja.html",
+    "inicial": "/paginas/Inicial/inicial.html",
+    "ingressos": "/paginas/Ingressos/ingressos.html",
+    "loja": "/paginas/Loja/loja.html",
     "contatos": "#Footer",
     "instagram": "https://www.instagram.com/gatos_fofos_e_engracados/",
     "twitter": "https://twitter.com/gatinarios"
