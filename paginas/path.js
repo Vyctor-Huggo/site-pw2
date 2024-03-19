2a53cda578d164cdb390e7f7bf5152786ff98ef2
Vyctor Huggo <vyctorhuggo322@gmail.com>
2024-03-19 18:29:24 -0300

    aa

--- paginas/path.js
+++ paginas/path.js
@@ -1,7 +1,7 @@
 var paths = {
-    "inicial": "/paginas/Inicial/inicial.html",
-    "ingressos": "/paginas/Ingressos/ingressos.html",
-    "loja": "/paginas/Loja/loja.html",
+    "inicial": "../Inicial/inicial.html",
+    "ingressos": "../Ingressos/ingressos.html",
+    "loja": "../Loja/loja.html",
     "contatos": "#Footer",
     "instagram": "https://www.instagram.com/gatos_fofos_e_engracados/",
     "twitter": "https://twitter.com/gatinarios"
